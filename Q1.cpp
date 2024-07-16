9a7388684cdd6b4dcf1777a81621f2af6829238f
Rohit Parit <rohitparit1934@gmail.com>
2024-07-16 19:00:44 +0530

    testCode

---
+++ Q1.cpp
@@ -0,0 +1,12 @@
+#include <algorithm>
+#include <climits>
+#include <cmath>
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+int main() {
+    cout << "Hello codeSoft ";
+
+    return 0;
+}
