998c683a0ab39f587c12de970ba0329531d81605
Rohit Parit <rohitparit1934@gmail.com>
2024-07-20 18:04:09 +0530

    simple cal

---
+++ simpleCalculator.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <climits>
+#include <vector>
+#include <cmath>
+using namespace std;
+class Calculator
+{
+public:
+    int a;
+    int b;
+
+    Calculator operator+(Calculator &obj1)
+    {
+    }
+};
+
+int main()
+{
+}
